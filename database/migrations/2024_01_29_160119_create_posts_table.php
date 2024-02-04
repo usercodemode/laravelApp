05a7919fd625095c93075b6d701d8947134cef95
Codemode <147582140+usercodemode@users.noreply.github.com>
2024-02-04 11:28:38 +0530

    create database/migrations/2024_01_29_160119_create_posts_table.php

---
+++ database/migrations/2024_01_29_160119_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default('');
+            $table->string('body')->default('');
+            $table->string('image_path')->default();
+            $table->boolean('is_published')->default(1);
+            $table->string('key')->nullable();
+            $table->foreignId('user_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
